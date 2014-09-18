ea7e416e680ac9ddaf26d4478e4c1a9d0f93cd3d
Mike Yachanin <mryachanin@gmail.com>
2014-09-18 14:57:10 -0400

    updated models

--- status/models.py
+++ status/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Category(models.Model):
+	"""Contains a category and parent category"""
+	
+	def __str__(self):
+		return self.name + " :: " + self.parent.name
+
+	name = models.CharField(max_length=20)
+	parent = models.ForeignKey(Category)
+
+
+class Post(models.Model):
+	"""Generic post object used by Status and Comment"""
+	
+	def __str__(self):
+		return self.created + " :: " + self.user + " :: " + self.message
+
+	created = models.DateField(auto_now = False, auto_now_add = True)
+	updated = models.DateField(auto_now = True)
+	message = models.TextField()
+	user = models.ForeignKey(User)
+
+
+class Status(Post):
+	"""Stores status posts"""
+
+	status = SmallPositiveIntegerField()
+	category = models.ForeignKey(Category)
+
+
+class Comment(Post):
+	"""Stores comment posts"""
+
+	status = models.ForeignKey(Status)
