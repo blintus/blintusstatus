2b5c3dfe76c4f0a7050acc73564d8acebc4287eb
Mike Yachanin <mryachanin@gmail.com>
2014-09-18 17:38:42 -0400

    registered models with admin interface

--- status/admin.py
+++ status/admin.py
@@ -2,5 +2,10 @@ from django.contrib import admin
 
 # Register your models here.
 
-from status.models import Category
+from status.models import *
 admin.site.register(Category)
+admin.site.register(Post)
+admin.site.register(Status)
+admin.site.register(Comment)
+admin.site.register(Provider)
+admin.site.register(ContactMethod)
