a7c370ef0bb4d0fff5ca613a05a4900c7f3a93a9
Dave Korhumel <dave.korhumel@oracle.com>
2014-09-18 15:18:47 -0400

    Added bootstrap and a basic template. Updated the configuration to support static files

--- status/urls.py
+++ status/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 urlpatterns = patterns('status',
 
-    url(r'^$', 'views.index')
+    url(r'^rest', 'views.index'),
+    url(r'^', TemplateView.as_view(template_name='status/page.html'))
 )
