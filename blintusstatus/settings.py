dd3233261e0101da7ad1784118b8e236a57bc7d5
David Korhumel <dpk2442@gmail.com>
2014-09-18 20:46:27 -0400

    Moved static assets into status app

--- blintusstatus/settings.py
+++ blintusstatus/settings.py
@@ -86,8 +86,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'assets'),
+    os.path.join(BASE_DIR, 'status/assets'),
 )
 STATIC_URL = '/assets/'
