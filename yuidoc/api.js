9be3bb8afd3931bf218ab91aa33c62010595528e
David Korhumel <dpk2442@gmail.com>
2014-09-27 16:13:50 -0400

    Generated yuidocs for the project

---
+++ yuidoc/api.js
@@ -0,0 +1,11 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "HomeController",
+        "HomeView",
+        "Store"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
