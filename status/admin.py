7e728ead8f5a2bbfe97e2a63990ad07a3cda269f
Mike Yachanin <mryachanin@gmail.com>
2014-09-27 16:25:04 -0400

    django admin un-fucked

--- status/admin.py
+++ status/admin.py
@@ -9,6 +9,6 @@ admin.site.register(Provider)
 admin.site.register(ContactMethod)
 
 class StatusAdmin(admin.ModelAdmin):
-    fields = ('title', 'category', 'post', 'user', 'message')
+    fields = ('title', 'category', 'status', 'user', 'message')
 
 admin.site.register(Post, StatusAdmin)
