6de5af479a1d1188b6953a81f8bc40566baab691
David Korhumel <dpk2442@gmail.com>
2014-10-25 02:33:32 -0400

    Added logic to update category statuses on post or category save

--- status/admin.py
+++ status/admin.py
@@ -3,13 +3,44 @@ from django.contrib import admin
 # Register your models here.
 
 from status.models import *
-admin.site.register(Category)
+
+
+def updateCategoryStatuses(category, status):
+        category.status = status
+        category.save()
+        if category.parent == None:
+            return
+        if status == 2:
+            updateCategoryStatuses(category.parent, 2)
+        else:
+            siblingCategories = Category.objects.filter(parent = category.parent)
+            siblingsMatch = True
+            for sibling in siblingCategories:
+                if sibling.status != status:
+                    siblingsMatch = False
+                    break
+            if siblingsMatch:
+                updateCategoryStatuses(category.parent, status)
+            else:
+                updateCategoryStatuses(category.parent, 2)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        updateCategoryStatuses(obj, obj.status)
+
+
+class PostAdmin(admin.ModelAdmin):
+    fields = ('title', 'category', 'status', 'user', 'message')
+
+    def save_model(self, request, obj, form, change):
+        updateCategoryStatuses(obj.category, obj.status)
+        obj.save();
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Comment)
 admin.site.register(Provider)
 admin.site.register(ContactMethod)
 admin.site.register(Subscription)
-
-class StatusAdmin(admin.ModelAdmin):
-    fields = ('title', 'category', 'status', 'user', 'message')
-
-admin.site.register(Post, StatusAdmin)
+admin.site.register(Post, PostAdmin)
